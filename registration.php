bec6cbb955694dba9a913cc0079ff1d7f894a1c9
Mayur Dhudasia <mmdhudasia@gmail.com>
2020-05-16 15:27:02 +0530

    initial

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Most Viewed Product Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_MostViewed
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_MostViewed',
+    __DIR__
+);
